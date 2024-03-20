f2b3decf0cc1ee1f51406492f7aa4620b98b0ee7
lab_services_student <lab_services_student@labVMH8OX>
2024-03-20 15:41:50 +0200

    Add project files.

---
+++ Tesing/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tesing.Models;
+
+namespace Tesing.Controllers
+{
+    public class UserController : Controller
+    {
+        public Table_1 usrtbl = new Table_1();
+
+
+        [HttpPost]
+
+        public ActionResult About(Table_1 Users)
+        {
+            var result = usrtbl.insert_User(Users);
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpGet]
+
+        public ActionResult About()
+        {
+            return View(usrtbl);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tesing/Models/Table_1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace Tesing.Models
+{
+    public class Table_1
+
+    {
+        public static string con_string = " Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=test;Data Source=labVMH8OX\\SQLEXPRESS";
+
+        //public static string con_string = " Server=tcp:cldv-sql-server2024.database.windows.net,1433;Initial Catalog=cldv-db;Persist Security Info=False;User ID=PillayK;Password= Kylepill@y07;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+
+        public static SqlConnection con = new SqlConnection(con_string);
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public string email { get; set; }
+
+        public string password { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public int insert_User(Table_1 m) // insert_product
+        {
+            string sql = "INSERT INTO userTable (userName, userSurname, userEmail) VALUES (@Name, @Suranme, @Email)";
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@Name", m.Name);
+            cmd.Parameters.AddWithValue("@Surname",m.Surname); // each colum name e.g. product description 
+            cmd.Parameters.AddWithValue("@Email",m.email);
+            con.Open();
+            int rowsAffected = cmd.ExecuteNonQuery();
+            con.Close();
+            return rowsAffected;
+
+        }
+    }
+}
